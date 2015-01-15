53b047f008abed14ef81baba7ddaeba75113ad6a
gerardogtn <gerardogtn@gmail.com>
2015-01-15 16:15:16 -0600

    Project created
    Project is empty

---
+++ hanoi_recursive/hanoi_recursive/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hanoi_recursive
+//
+//  Created by Gerardo Teruel on 1/15/15.
+//  Copyright (c) 2015 Gerardo Teruel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
