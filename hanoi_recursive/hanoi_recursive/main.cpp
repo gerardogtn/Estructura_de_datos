cecf4b02256030520be32cc8da8e39060f7425f7
gerardogtn <gerardogtn@gmail.com>
2015-01-15 16:40:51 -0600

    Readable
    Added comments to improve documentation and separated main function in
    parts to improve readability.

--- hanoi_recursive/hanoi_recursive/main.cpp
+++ hanoi_recursive/hanoi_recursive/main.cpp
@@ -10,7 +10,14 @@
 using namespace std;
 
 void hanoi(int, int, int, int);
+int isNumDisksValid(int);
 
+/*
+ * Initializes the hanoi-solving algorithm. 
+ * a) Prompts the user for the number of disks on the game
+ * b) Verifies that the number of disks is an integer greater than one. 
+ * c) Assumes that there are only three hanoi sticks.
+ */
 int main(int argc, const char * argv[]) {
     
     int numDisks;
@@ -18,16 +25,17 @@ int main(int argc, const char * argv[]) {
     cout << "Input the number of disks: ";
     cin >> numDisks;
     
-    if (numDisks < 1){
-        cout << "The number of disks must be greater than zero. ";
-        cin >> numDisks;
-    }
-    
+    numDisks = isNumDisksValid(numDisks);
     hanoi(numDisks, 1, 3, 2);
     
     return 0;
 }
 
+/* 
+ * REQUIRES: origin, destination, and aux are three consecutive integers that CANNOT be the same
+ * MODIFIES: None
+ * EFFECTS: Notifies the user of the necessary movements to solve the hanoi game.
+ */
 void hanoi(int numDisks, int origin, int destination, int aux){
     
     if (numDisks == 1){
@@ -38,4 +46,18 @@ void hanoi(int numDisks, int origin, int destination, int aux){
         cout << " Move the disk " << numDisks << " from " << origin << " to " << destination << endl;
         hanoi(numDisks -1, aux, destination, origin);
     }
+}
+/*
+ * REQUIRES: None
+ * MODIFIES: None
+ * EFFECTS:  If the parameter is less than one, prompts the user to enter another integer. 
+ *           Otherwise, it returns the value of the parameter. 
+ */
+int isNumDisksValid(int numDisks){
+    if (numDisks < 1){
+        cout << "The number of disks must be greater than zero. ";
+        cin >> numDisks;
+        return numDisks;
+    }
+    else return numDisks;
 }
