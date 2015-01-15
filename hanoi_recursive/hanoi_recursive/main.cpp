e4373e792943ff1d9035bfefab69b5c63bfa5078
gerardogtn <gerardogtn@gmail.com>
2015-01-15 16:19:28 -0600

    functionality implemented
    Functionality was implemented. Program is working correctly

--- hanoi_recursive/hanoi_recursive/main.cpp
+++ hanoi_recursive/hanoi_recursive/main.cpp
@@ -7,9 +7,35 @@
 //
 
 #include <iostream>
+using namespace std;
+
+void hanoi(int, int, int, int);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    int numDisks;
+    
+    cout << "Input the number of disks: ";
+    cin >> numDisks;
+    
+    if (numDisks < 1){
+        cout << "The number of disks must be greater than zero. ";
+        cin >> numDisks;
+    }
+    
+    hanoi(numDisks, 1, 3, 2);
+    
     return 0;
+}
+
+void hanoi(int numDisks, int origin, int destination, int aux){
+    
+    if (numDisks == 1){
+        cout << " Move the disk " << numDisks << " from " << origin << " to " << destination << endl;
+    }
+    else{
+        hanoi(numDisks - 1, origin, aux, destination);
+        cout << " Move the disk " << numDisks << " from " << origin << " to " << destination << endl;
+        hanoi(numDisks -1, aux, destination, origin);
+    }
 }
